ed74c0abfb9ed8a7392700c19ac852d7e53f4d00
elizaamanda <elizaamandaksp79@gmail.com>
2024-09-26 12:22:41 +0700

    upload project dasar

---
+++ harike-12/post.php
@@ -0,0 +1,15 @@
+<!DOCTYPE HTML>
+<html lang='en'>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"content="width=device-width,initial-scale=1.0">
+    <title>NIP</title>
+</head>
+<body>
+    <form action="index.php" method="post">
+    <input type="text" name="nip" id="nip">
+    <button type="submit">proses</button>
+</form>
+</body>
+</html>
+    
